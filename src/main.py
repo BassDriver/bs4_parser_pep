ba3ff432a6e77f66433cc04ed0813de0405b0442
Сергей Шубин <bassdriver83@yandex.ru>
2023-03-22 22:31:23 +0300

    Sprint_18_final_v1

--- src/main.py
+++ src/main.py
@@ -105,23 +105,13 @@ def pep(session):
     for pep_line in tqdm(pep_lines):
         total_pep_count += 1
         short_status = pep_line.find('td').text[1:]
-        try:
-            status_ext = EXPECTED_STATUS[short_status]
-        except KeyError:
-            status_ext = {}
-            logging.info(
-                f'\nОшибочный статус в общем списке: {short_status}\n'
-                f'Строка PEP: {pep_line}'
-            )
+        status_ext = EXPECTED_STATUS[short_status]
         link = find_tag(pep_line, 'a')['href']
         full_link = urljoin(PEP_LIST_URL, link)
         response = get_response(session, full_link)
         soup = BeautifulSoup(response.text, 'lxml')
         dl_tag = find_tag(soup, 'dl')
         status_line = dl_tag.find(string='Status')
-        if not status_line:
-            logging.error(f'{full_link} - не найдена строка статуса')
-            continue
         status_line = status_line.find_parent()
         status_int = str(status_line.next_sibling.next_sibling.string)
         if status_int not in status_ext:
@@ -137,16 +127,7 @@ def pep(session):
             status_counter[status_int] = 1
     for status in status_counter:
         results.append((status, status_counter[status]))
-    sum_from_cards = sum(status_counter.values())
-    if total_pep_count != sum_from_cards:
-        logging.error(
-            f'\n Ошибка в сумме:\n'
-            f'Всего PEP: {total_pep_count}'
-            f'Всего статусов из карточек: {sum_from_cards}'
-        )
-        results.append(('Total', sum_from_cards))
-    else:
-        results.append(('Total', total_pep_count))
+    results.append(('Total', total_pep_count))
     return results
 
 
